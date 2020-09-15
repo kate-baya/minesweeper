b06adbe0a1b8611977acc0c80dc610d623f6f103
Kate-Baya <ksbaya@gmail.com>
2020-09-15 19:34:44 +1200

    added styling

--- minesweeper.js
+++ minesweeper.js
@@ -1,47 +1,211 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
-// Define your `board` object here!
-var board = {
-  cells: [
-    {col: 1, 
-    row: 1,
-    isMine: true,
-    hidden: true}, 
-    {col: 1, 
-    row: 2,
-    isMine: false,
-    hidden: true}, 
-    {col: 1,
-    row: 3,
-    isMine: false,
-    hidden: true}, 
-    {col: 2,
-    row: 1,
-    isMine: false,
-    hidden: true}, 
-    {col: 2,
-    row: 2,
-    isMine: false,
-    hidden: true}, 
-    {col: 2,
-    row: 3,
-    isMine: false,
-    hidden: true}, 
-    {col: 3,
-    row: 1,
-    isMine: false,
-    hidden: true}, 
-    {col: 3,
-    row: 2,
-    isMine: false,
-    hidden: true}, 
-    {col: 3,
-    row: 3,
-    isMine: false,
-    hidden: true}]
+var cells = []
+var col = 0
+var row = 0
+var hidden = true
+
+var board = {}
+
+var winAudio = new Audio()
+winAudio.src = "cleverGirl_1.wav"
+
+var loseAudio = new Audio()
+loseAudio.src = "eaten_1.mp3"
+
+//easy
+function createBoard() {
+  board.cells = []
+  do {
+    var randomNum = Math.floor((Math.random() * 10) + 1)
+    if (randomNum % 4 == 0) {
+      var isMine = true
+      } else isMine = false
+
+    board.cells.push({col, row, isMine, hidden});
+    col++
+  } 
+  while (board.cells.length < 4)
+  row = 1
+  col = 0
+  do {
+    var randomNum = Math.floor((Math.random() * 10) + 1)
+    if (randomNum % 4 == 0) {
+      var isMine = true
+      } else isMine = false
+
+    board.cells.push({col, row, isMine, hidden});
+    col++
+  } 
+  while (board.cells.length < 8)
+  row = 2
+  col = 0
+  do {
+    var randomNum = Math.floor((Math.random() * 10) + 1)
+    if (randomNum % 4 == 0) {
+      var isMine = true
+      } else isMine = false
+
+    board.cells.push({col, row, isMine, hidden});
+    col++
+  } 
+  while (board.cells.length < 12)
+  row = 3
+  col = 0
+  do {
+    var randomNum = Math.floor((Math.random() * 10) + 1)
+    if (randomNum % 4 == 0) {
+      var isMine = true
+      } else isMine = false
+
+    board.cells.push({col, row, isMine, hidden});
+    col++
+  } 
+  while (board.cells.length < 16)
+  console.log(board)
 }
 
+// //meduim
+// function createBoard() {
+//   board.cells = []
+//   do {
+//     var randomNum = Math.floor((Math.random() * 10) + 1)
+//     if (randomNum % 3 == 0) {
+//       var isMine = true
+//       } else isMine = false
+
+//     board.cells.push({col, row, isMine, hidden});
+//     col++
+//   } 
+//   while (board.cells.length < 5)
+//   row = 1
+//   col = 0
+//   do {
+//     var randomNum = Math.floor((Math.random() * 10) + 1)
+//     if (randomNum % 3 == 0) {
+//       var isMine = true
+//       } else isMine = false
+
+//     board.cells.push({col, row, isMine, hidden});
+//     col++
+//   } 
+//   while (board.cells.length < 10)
+//   row = 2
+//   col = 0
+//   do {
+//     var randomNum = Math.floor((Math.random() * 10) + 1)
+//     if (randomNum % 3 == 0) {
+//       var isMine = true
+//       } else isMine = false
+
+//     board.cells.push({col, row, isMine, hidden});
+//     col++
+//   } 
+//   while (board.cells.length < 15)
+//   row = 3
+//   col = 0
+//   do {
+//     var randomNum = Math.floor((Math.random() * 10) + 1)
+//     if (randomNum % 3 == 0) {
+//       var isMine = true
+//       } else isMine = false
+
+//     board.cells.push({col, row, isMine, hidden});
+//     col++
+//   } 
+//   while (board.cells.length < 20)
+//   row = 4
+//   col = 0
+//   do {
+//     var randomNum = Math.floor((Math.random() * 10) + 1)
+//     if (randomNum % 3 == 0) {
+//       var isMine = true
+//       } else isMine = false
+
+//     board.cells.push({col, row, isMine, hidden});
+//     col++
+//   } 
+//   while (board.cells.length < 25)
+//   console.log(board)
+// }
+
+//hard
+// function createBoard() {
+//   board.cells = []
+//   do {
+//     var randomNum = Math.floor((Math.random() * 10) + 1)
+//     if (randomNum % 2 == 0) {
+//       var isMine = true
+//       } else isMine = false
+
+//     board.cells.push({col, row, isMine, hidden});
+//     col++
+//   } 
+//   while (board.cells.length < 6)
+//   row = 1
+//   col = 0
+//   do {
+//     var randomNum = Math.floor((Math.random() * 10) + 1)
+//     if (randomNum % 2 == 0) {
+//       var isMine = true
+//       } else isMine = false
+
+//     board.cells.push({col, row, isMine, hidden});
+//     col++
+//   } 
+//   while (board.cells.length < 12)
+//   row = 2
+//   col = 0
+//   do {
+//     var randomNum = Math.floor((Math.random() * 10) + 1)
+//     if (randomNum % 2 == 0) {
+//       var isMine = true
+//       } else isMine = false
+
+//     board.cells.push({col, row, isMine, hidden});
+//     col++
+//   } 
+//   while (board.cells.length < 18)
+//   row = 3
+//   col = 0
+//   do {
+//     var randomNum = Math.floor((Math.random() * 10) + 1)
+//     if (randomNum % 2 == 0) {
+//       var isMine = true
+//       } else isMine = false
+
+//     board.cells.push({col, row, isMine, hidden});
+//     col++
+//   } 
+//   while (board.cells.length < 24)
+//   row = 4
+//   col = 0
+//   do {
+//     var randomNum = Math.floor((Math.random() * 10) + 1)
+//     if (randomNum % 2 == 0) {
+//       var isMine = true
+//       } else isMine = false
+
+//     board.cells.push({col, row, isMine, hidden});
+//     col++
+//   } 
+//   while (board.cells.length < 30)
+//   row = 5
+//   col = 0
+//   do {
+//     var randomNum = Math.floor((Math.random() * 10) + 1)
+//     if (randomNum % 2 == 0) {
+//       var isMine = true
+//       } else isMine = false
+
+//     board.cells.push({col, row, isMine, hidden});
+//     col++
+//   } 
+//   while (board.cells.length < 36)
+// }
+
 function startGame () {
+  createBoard()
   for (var i = 0; i < board.cells.length; i++) {
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
     }
@@ -51,6 +215,12 @@ function startGame () {
   lib.initBoard()
 
 }
+
+function resetBoard () {
+  delete board.cells
+  startGame()
+}
+document.getElementById("reset").onclick = resetBoard
 
 // Define this function to look for a win condition:
 //
@@ -62,11 +232,12 @@ function checkForWin () {
       return
     } else if (board.cells[x].isMine === false && board.cells[x].hidden === true) {
       return 
-    }
+    } 
   }  
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
   lib.displayMessage('You win!')
+  winAudio.play()
 }
 
 // Define this function to count the number of mines around the cell
